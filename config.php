ea8854e0b0854bc9c48004c9673f3b7814dad03f
2200243 <2200243@usap.edu.test-google-a.com>
2022-08-11 12:05:29 -0600

    test db

--- config.php
+++ config.php
@@ -8,8 +8,9 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-$mysqli= new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_d);
+//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$mysqli = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//$mysqli= new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if ($mysqli->connect_errno) {
     echo $mysqli->connect_errno;
