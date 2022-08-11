97f9fdab0d78f8c940e2d472487ac4b302c2637a
2200243 <2200243@usap.edu.test-google-a.com>
2022-08-11 11:52:55 -0600

    add composer + config mysql

--- config.php
+++ config.php
@@ -1,15 +1,19 @@
 <?php
-
-
-$servidor = "localhost";
-$basedatos = "desarollo_aplicaciones";
-$usuario = "2200243";
-$password = "";
-$mysqli= new mysqli($servidor,$usuario,$password,$basedatos);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$mysqli= new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_d);
 
 if ($mysqli->connect_errno) {
     echo $mysqli->connect_errno;
 }
 
-$urlweb = "http://localhost/tiendavirtual/";
+$urlweb = "https://libreriamoiras.herokuapp.com/";
 ?>
